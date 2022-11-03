da302a0c26abea8068f401716a6785722d7343fa
Rogengell <101105996+Rogengell@users.noreply.github.com>
2022-11-03 13:23:20 +0100

    Add project files.

---
+++ Week44Project/ViewModel/AddCommands.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week44Project.ViewModel
+{
+    internal class AddCommands
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        public void RaisCanExecuteChanged() => this.CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            this.actionToInvoke(parameter);
+        }
+
+        private Action<Object> actionToInvoke = null;
+        public AddCommands(Action<Object> actionToInvoke)
+        {
+            this.actionToInvoke = actionToInvoke;
+        }
+    }
+}
---
+++ Week44Project/ViewModel/ViewModelBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week44Project.ViewModel
+{
+    internal class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void PropertyIsChanged([CallerMemberName] string memberName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(memberName));
+        }
+    }
+}
