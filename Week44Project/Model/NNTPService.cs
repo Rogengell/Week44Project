8a475c93191ab7b7cbe9f9db8dbe6616fd229de3
Rogengell <101105996+Rogengell@users.noreply.github.com>
2022-11-03 15:23:37 +0100

    g

--- Week44Project/Model/NNTPService.cs
+++ Week44Project/Model/NNTPService.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,46 @@ namespace Week44Project.Model
 {
     internal class NNTPService
     {
+        public TcpClient socket { get; set; }
+        public NetworkStream ns { get; set; }
+        public StreamReader reader { get; set; }
+
+        public bool connectConfirm { get; set; }
+
+        public NNTPService()
+        {
+            socket = null;
+            ns = null;
+            reader = null;
+        }
+        public void ConnectNTTP()
+        {
+            String serverName = "news.dotsrc.org";
+            int serverPort = 119;
+            connectConfirm = false;
+            try
+            {
+                socket = new TcpClient(serverName, serverPort);
+
+                ns = socket.GetStream();
+
+                reader = new StreamReader(ns, Encoding.UTF8);
+
+                Console.WriteLine(reader.ReadLine());
+                ns.Flush();
+                connectConfirm = true;
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                socket.Close();
+                ns.Close();
+                reader.Close();
+            }   
+        }
     }
 }
--- Week44Project/ViewModel/NNTPViewModel.cs
+++ Week44Project/ViewModel/NNTPViewModel.cs
@@ -1,13 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using Week44Project.Model;
 
 namespace Week44Project.ViewModel
 {
     internal class NNTPViewModel : ViewModelBase
     {
+        private string name = "";
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; PropertyIsChanged(); }
+        }
+
+        private string pass = "";
+
+        public string Pass
+        {
+            get { return pass; }
+            set { pass = value; PropertyIsChanged(); }
+        }
+
+        private string connectStatus;
+
+        public string ConnectStatus
+        {
+            get { return connectStatus; }
+            set { connectStatus = value; PropertyIsChanged(); }
+        }
+
+
+        public AddCommands login { get; set; }
+
+        private NNTPService Service;
+
+        public NNTPViewModel()
+        {
+            this.Service = new NNTPService();
+            login = new AddCommands(Login);
+        }
+
+        public void Login(object parameter)
+        {
+            Service.ConnectNTTP();
+            if (Service.connectConfirm)
+            {
+                ConnectStatus = "Server Ready";
+            }
+            else
+            {
+                ConnectStatus = "Sever Ready";
+            }
+        }
+
+        
+
+
 
 
     }
